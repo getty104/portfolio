3e6e37cfdda294bafab392cad17523d1cc92ae6c
getty104 <hayabusatoshihumi@gmail.com>
2018-12-03 12:52:12 +0900

    Create Home basic design

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,11 +3,11 @@ import * as React from "react";
 export class Home extends React.Component {
   public render() {
     return (
-      <div className="container">
-        <div className="hm-Bar hm-Bar_profile" />
-        <div className="hm-Bar hm-Bar_skill" />
-        <div className="hm-Bar hm-Bar_contents" />
-        <div className="hm-Bar hm-Bar_access" />
+      <div className="hm-Bar">
+        <div className="hm-Bar_profile" />
+        <div className="hm-Bar_skill" />
+        <div className="hm-Bar_contents" />
+        <div className="hm-Bar_access" />
       </div>
     );
   }
