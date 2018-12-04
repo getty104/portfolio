ac938d19fd459003af4aca961ba5a1da092e7b36
getty104 <hayabusatoshihumi@gmail.com>
2018-12-04 15:17:32 +0900

    add favicons

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,23 +3,28 @@ import * as React from "react";
 export class Home extends React.Component {
   public render() {
     return (
-      <div className="hm-Content">
-        <a className="hm-Content_profile" href="#">
-          <div className="hm-Content_profile-bar" />
-          <div className="hm-Content_profile-title">自己紹介</div>
-        </a>
-        <a className="hm-Content_skill" href="#">
-          <div className="hm-Content_skill-bar" />
-          <div className="hm-Content_skill-title">スキル</div>
-        </a>
-        <a className="hm-Content_access" href="#">
-          <div className="hm-Content_access-bar" />
-          <div className="hm-Content_access-title">アクセス</div>
-        </a>
-        <a className="hm-Content_blog" href="#">
-          <div className="hm-Content_blog-bar" />
-          <div className="hm-Content_blog-title">ブログ</div>
-        </a>
+      <div>
+        <div className="hm-Content">
+          <a className="hm-Content_profile" href="#">
+            <div className="hm-Content_profile-bar" />
+            <div className="hm-Content_profile-title">自己紹介</div>
+          </a>
+          <a className="hm-Content_skill" href="#">
+            <div className="hm-Content_skill-bar" />
+            <div className="hm-Content_skill-title">スキル</div>
+          </a>
+          <a className="hm-Content_access" href="#">
+            <div className="hm-Content_access-bar" />
+            <div className="hm-Content_access-title">アクセス</div>
+          </a>
+          <a className="hm-Content_blog" href="#">
+            <div className="hm-Content_blog-bar" />
+            <div className="hm-Content_blog-title">ブログ</div>
+          </a>
+        </div>
+        <div className="hm-Footer">
+          <div className="hm-Footer_column">Getty's Room</div>
+        </div>
       </div>
     );
   }
