bf141daea33bb8058d163dd50dcc6fdd9b613993
getty104 <hayabusatoshihumi@gmail.com>
2018-12-04 11:12:02 +0900

    Refactor styles

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,22 +3,22 @@ import * as React from "react";
 export class Home extends React.Component {
   public render() {
     return (
-      <div className="hm-Bar">
-        <a className="hm-Bar_profile" href="#">
-          <div className="hm-Bar_profile-bar" />
-          <div className="hm-Bar_profile-title">自己紹介</div>
+      <div className="hm-Content">
+        <a className="hm-Content_profile" href="#">
+          <div className="hm-Content_profile-bar" />
+          <div className="hm-Content_profile-title">自己紹介</div>
         </a>
-        <a className="hm-Bar_skill" href="#">
-          <div className="hm-Bar_skill-bar" />
-          <div className="hm-Bar_skill-title">スキル</div>
+        <a className="hm-Content_skill" href="#">
+          <div className="hm-Content_skill-bar" />
+          <div className="hm-Content_skill-title">スキル</div>
         </a>
-        <a className="hm-Bar_contents" href="#">
-          <div className="hm-Bar_contents-bar" />
-          <div className="hm-Bar_contents-title">ブログ</div>
+        <a className="hm-Content_blog" href="#">
+          <div className="hm-Content_blog-bar" />
+          <div className="hm-Content_blog-title">ブログ</div>
         </a>
-        <a className="hm-Bar_access" href="#">
-          <div className="hm-Bar_access-bar" />
-          <div className="hm-Bar_access-title">アクセス</div>
+        <a className="hm-Content_access" href="#">
+          <div className="hm-Content_access-bar" />
+          <div className="hm-Content_access-title">アクセス</div>
         </a>
       </div>
     );
