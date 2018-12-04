b03ed8be64f06a6013182a1bac281931d7ba2b6d
getty104 <hayabusatoshihumi@gmail.com>
2018-12-04 11:08:50 +0900

    improve UI

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,10 +4,22 @@ export class Home extends React.Component {
   public render() {
     return (
       <div className="hm-Bar">
-        <div className="hm-Bar_profile" />
-        <div className="hm-Bar_skill" />
-        <div className="hm-Bar_contents" />
-        <div className="hm-Bar_access" />
+        <a className="hm-Bar_profile" href="#">
+          <div className="hm-Bar_profile-bar" />
+          <div className="hm-Bar_profile-title">自己紹介</div>
+        </a>
+        <a className="hm-Bar_skill" href="#">
+          <div className="hm-Bar_skill-bar" />
+          <div className="hm-Bar_skill-title">スキル</div>
+        </a>
+        <a className="hm-Bar_contents" href="#">
+          <div className="hm-Bar_contents-bar" />
+          <div className="hm-Bar_contents-title">ブログ</div>
+        </a>
+        <a className="hm-Bar_access" href="#">
+          <div className="hm-Bar_access-bar" />
+          <div className="hm-Bar_access-title">アクセス</div>
+        </a>
       </div>
     );
   }
