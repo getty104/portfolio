2dcbcb7316cc597852d8e3534e8c3a0b29288cb1
getty104 <hayabusatoshihumi@gmail.com>
2019-01-03 18:31:07 +0900

    Fix BlogShow for rendering NotFound when post does not exist

--- src/components/BlogShow.tsx
+++ src/components/BlogShow.tsx
@@ -7,9 +7,11 @@ import { blogsPath } from "../routes";
 import { formatDate } from "../tools/formatDate";
 import { executeScriptTagsById } from "../tools/scriptTagExecuter";
 import { GetPostQuery } from "../types/graphql";
+import { NotFound } from "./NotFound";
 
 interface State {
   post: GetPostQuery["post"];
+  isInitialized: boolean;
 }
 
 interface Props {
@@ -23,57 +25,60 @@ interface Props {
 export class BlogShow extends React.Component<Props, State> {
   constructor(props: Props) {
     super(props);
-    this.state = { post: null };
+    this.state = { post: null, isInitialized: false };
   }
 
   public componentDidMount() {
     window.scrollTo(0, 0);
     getPost(this.props.match.params.id).then(result => {
-      result.success &&
-        result.data &&
-        result.data.post &&
-        this.setState({ post: result.data.post });
+      result.success && result.data && result.data.post
+        ? this.setState({ post: result.data.post, isInitialized: true })
+        : this.setState({ isInitialized: true });
       setTimeout(() => executeScriptTagsById("blog"), 100);
     });
   }
 
   public render() {
-    return (
-      <div id="blog">
-        {this.state.post && (
-          <div>
-            <div className="bgs-Content">
-              <div className="bgs-Content_header">
-                <div className="bgs-Content_header-title">
-                  <div className="bgs-Content_title-text">
-                    {this.state.post.title}
+    if (this.state.isInitialized && !this.state.post) {
+      return <NotFound />;
+    } else {
+      return (
+        <div id="blog">
+          {this.state.post && (
+            <div>
+              <div className="bgs-Content">
+                <div className="bgs-Content_header">
+                  <div className="bgs-Content_header-title">
+                    <div className="bgs-Content_title-text">
+                      {this.state.post.title}
+                    </div>
+                    <div className="bgs-Content_title-date">
+                      {formatDate(this.state.post.insertedAt)}
+                    </div>
                   </div>
-                  <div className="bgs-Content_title-date">
-                    {formatDate(this.state.post.insertedAt)}
-                  </div>
+                  {this.state.post.image && (
+                    <img
+                      className="bgs-Content_header-image"
+                      src={this.state.post.image}
+                    />
+                  )}
                 </div>
-                {this.state.post.image && (
-                  <img
-                    className="bgs-Content_header-image"
-                    src={this.state.post.image}
-                  />
-                )}
+                <div
+                  className="bgs-Content_description bgs-Markdown"
+                  dangerouslySetInnerHTML={{
+                    __html: getParsedMarkdownText(this.state.post.body)
+                  }}
+                />
               </div>
-              <div
-                className="bgs-Content_description bgs-Markdown"
-                dangerouslySetInnerHTML={{
-                  __html: getParsedMarkdownText(this.state.post.body)
-                }}
-              />
+              <div className="bgs-Footer">
+                <Link to={blogsPath()} className="bgs-Footer_column">
+                  一覧へ
+                </Link>
+              </div>
             </div>
-            <div className="bgs-Footer">
-              <Link to={blogsPath()} className="bgs-Footer_column">
-                一覧へ
-              </Link>
-            </div>
-          </div>
-        )}
-      </div>
-    );
+          )}
+        </div>
+      );
+    }
   }
 }
