af5025ea4e4fa7d871646c552a781b691c9629f5
getty104 <hayabusatoshihumi@gmail.com>
2019-02-04 14:38:10 +0900

    Add informationn of qiita account to 'Contact' page

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import { contactPath, productsPath, rootPath, selfIntroPath } from "../routes";
 import facebook from "../styles/images/facebook.png";
 import github from "../styles/images/github.png";
+import qiita from "../styles/images/qiita-favicon.png";
 import twitter from "../styles/images/twitter.png";
 import { Head } from "./Head";
 
@@ -22,6 +23,7 @@ export class Contact extends React.Component {
         <div className="ct-Content">
           <div className="ct-Content_title">コンタクト</div>
           <div className="ct-Content_bar" />
+          <div className="ct-Content_category">SNS関連</div>
           <div className="ct-Content_description">
             <a
               href="https://www.facebook.com/profile.php?id=100009972646027"
@@ -34,6 +36,9 @@ export class Contact extends React.Component {
             </a>
             <a href="https://github.com/getty104" target="_blank">
               <img src={github} className="ct-Content_description-image" />
+            </a>
+            <a href="https://qiita.com/getty104" target="_blank">
+              <img src={qiita} className="ct-Content_description-image" />
             </a>
           </div>
         </div>
