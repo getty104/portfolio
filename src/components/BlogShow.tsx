237bae6f7053455bd11bff017f2baac023a86b00
getty104 <hayabusatoshihumi@gmail.com>
2019-03-17 23:46:05 +0900

    Refactor BlogShow with React Hooks

--- src/components/BlogShow.tsx
+++ src/components/BlogShow.tsx
@@ -6,6 +6,7 @@ import { blogPath, blogsPath } from "../routes";
 import { formatDate } from "../tools/formatDate";
 import { parseMarkdownText } from "../tools/parseMarkdownText";
 import { executeScriptTagsById } from "../tools/scriptTagExecuter";
+import { ActionType } from "../types/global";
 import { GetPostQuery } from "../types/graphql";
 import { Head } from "./Head";
 import { NotFound } from "./NotFound";
@@ -13,6 +14,11 @@ import { NotFound } from "./NotFound";
 interface State {
   post: GetPostQuery["post"];
   isInitialized: boolean;
+}
+
+interface Actions {
+  setPost: (posts: GetPostQuery["post"]) => ActionType<State>;
+  setIsInitialized: (isInitialized: boolean) => ActionType<State>;
 }
 
 interface Props {
@@ -23,69 +29,84 @@ interface Props {
   };
 }
 
-export class BlogShow extends React.Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
-    this.state = { post: null, isInitialized: false };
-  }
+const initialState: State = {
+  post: null,
+  isInitialized: false
+};
 
-  public componentDidMount() {
-    window.scrollTo(0, 0);
-    getPost(this.props.match.params.id).then(result => {
-      result.success && result.data && result.data.post
-        ? this.setState({ post: result.data.post, isInitialized: true })
-        : this.setState({ isInitialized: true });
+const actions: Actions = {
+  setPost: (post: GetPostQuery["post"]) => (state: State) => ({
+    ...state,
+    post
+  }),
+  setIsInitialized: (isInitialized: boolean) => (state: State) => ({
+    ...state,
+    isInitialized
+  })
+};
+
+const reducer = (state: State, action: ActionType<State>) => action(state);
+
+export const BlogShow = (props: Props) => {
+  const [state, dispatch] = React.useReducer(reducer, initialState);
+
+  React.useEffect(() => {
+    getPost(props.match.params.id).then(result => {
+      result.success &&
+        result.data &&
+        result.data.post &&
+        dispatch(actions.setPost(result.data.post));
+      dispatch(actions.setIsInitialized(true));
       setTimeout(() => executeScriptTagsById("blog"), 100);
+      return () => dispatch(actions.setIsInitialized(false));
     });
-  }
+  }, [props.match.params.id]);
 
-  public render() {
-    if (this.state.isInitialized && !this.state.post) {
-      return <NotFound />;
-    } else {
-      return (
-        <div id="blog">
-          {this.state.post && (
-            <div>
-              <Head
-                title={this.state.post.title}
-                description={this.state.post.body}
-                image={this.state.post.image}
-                url={blogPath({ id: this.props.match.params.id.toString() })}
-              />
-              <div className="bgs-Content">
-                <div className="bgs-Content_header">
-                  <div className="bgs-Content_header-title">
-                    <div className="bgs-Content_title-text">
-                      {this.state.post.title}
-                    </div>
-                    <div className="bgs-Content_title-date">
-                      {formatDate(this.state.post.insertedAt)}
-                    </div>
+  if (state.isInitialized && !state.post) {
+    return <NotFound />;
+  } else {
+    return (
+      <div id="blog">
+        {state.post && (
+          <div>
+            <Head
+              title={state.post.title}
+              description={state.post.body}
+              image={state.post.image}
+              url={blogPath({ id: props.match.params.id.toString() })}
+            />
+            <div className="bgs-Content">
+              <div className="bgs-Content_header">
+                <div className="bgs-Content_header-title">
+                  <div className="bgs-Content_title-text">
+                    {state.post.title}
                   </div>
-                  {this.state.post.image && (
-                    <img
-                      className="bgs-Content_header-image"
-                      src={this.state.post.image}
-                    />
-                  )}
+                  <div className="bgs-Content_title-date">
+                    {formatDate(state.post.insertedAt)}
+                  </div>
                 </div>
-                <div
-                  className="bgs-Content_description bgs-Markdown"
-                  dangerouslySetInnerHTML={{
-                    __html: parseMarkdownText(this.state.post.body)
-                  }}
-                />
+                {state.post.image && (
+                  <img
+                    className="bgs-Content_header-image"
+                    src={state.post.image}
+                  />
+                )}
               </div>
-              <div className="bgs-Footer">
-                <Link to={blogsPath()} className="bgs-Footer_column">
-                  一覧へ
-                </Link>
-              </div>
+              <div
+                className="bgs-Content_description bgs-Markdown"
+                dangerouslySetInnerHTML={{
+                  __html: parseMarkdownText(state.post.body)
+                }}
+              />
             </div>
-          )}
-        </div>
-      );
-    }
+            <div className="bgs-Footer">
+              <Link to={blogsPath()} className="bgs-Footer_column">
+                一覧へ
+              </Link>
+            </div>
+          </div>
+        )}
+      </div>
+    );
   }
-}
+};
