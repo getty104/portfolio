50f881eac86bdd25de0056fa59507f1b7b76f018
getty104 <hayabusatoshihumi@gmail.com>
2019-03-17 23:20:31 +0900

    Refactor BlogIndex with React Hooks

--- src/components/BlogIndex.tsx
+++ src/components/BlogIndex.tsx
@@ -5,6 +5,7 @@ import { Link } from "react-router-dom";
 import { getPosts } from "../interactors/getPosts";
 import { blogPath, blogsPath } from "../routes";
 import { formatDate } from "../tools/formatDate";
+import { ActionType } from "../types/global";
 import { GetPostsQuery } from "../types/graphql";
 import { Head } from "./Head";
 
@@ -13,97 +14,120 @@ interface State {
   currentPage: number;
 }
 
+interface Actions {
+  init: (props: Props) => ActionType<State>;
+  setPosts: (posts: GetPostsQuery["posts"]) => ActionType<State>;
+  setCurrentPage: (currentPage: number) => ActionType<State>;
+}
+
 interface Props {
   location: {
     search: string;
   };
 }
 
-export class BlogIndex extends React.Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
-    this.state = { posts: null, currentPage: 0 };
-    this.setPosts = this.setPosts.bind(this);
-  }
+const initialState: State = {
+  posts: null,
+  currentPage: 0
+};
 
-  public componentDidMount() {
-    window.scrollTo(0, 0);
-    const page = parse(this.props.location.search).page;
-    page
-      ? this.setPosts(parseInt(page instanceof Array ? page[0] : page, 10))
-      : this.setPosts(0);
-  }
+const actions: Actions = {
+  init: (props: Props) => (state: State) => {
+    const params = parse(props.location.search).page;
+    const currentPage = params
+      ? parseInt(params instanceof Array ? params[0] : params, 10)
+      : 0;
+    return {
+      ...state,
+      currentPage
+    };
+  },
+  setPosts: (posts: GetPostsQuery["posts"]) => (state: State) => ({
+    ...state,
+    posts
+  }),
+  setCurrentPage: (currentPage: number) => (state: State) => ({
+    ...state,
+    currentPage
+  })
+};
 
-  public render() {
-    return (
-      <div>
-        <Head
-          title={"ブログ"}
-          description={"Gettyのブログ"}
-          url={blogsPath()}
-        />
-        <div className="bgi-Content">
-          <div className="bgi-Content_title">ブログ</div>
-          <div className="bgi-Content_bar" />
-          <div className="bgi-Content_items">
-            {this.state.posts &&
-              this.state.posts.edges &&
-              this.state.posts.edges.map(
-                (post, index) =>
-                  post &&
-                  post.node && (
-                    <div className="bgi-Content_item" key={index}>
-                      <Link
-                        to={blogPath({ id: post.node.id })}
-                        className="bgi-Content_item-title"
-                      >
-                        {post.node.title}
-                      </Link>
-                      <div className="bgi-Content_item-date">
-                        {formatDate(post.node.insertedAt)}
-                      </div>
-                    </div>
-                  )
-              )}
-          </div>
-        </div>
-        <div className="bgi-Footer">
-          {this.state.posts && this.state.posts.pageInfo.hasPreviousPage && (
-            <Link
-              className="bgi-Footer_column"
-              to={{
-                pathname: blogsPath(),
-                search: `?page=${this.state.currentPage - 1}`
-              }}
-              onClick={() => this.setPosts(this.state.currentPage - 1)}
-            >
-              前のページ
-            </Link>
-          )}
-          {this.state.posts && this.state.posts.pageInfo.hasNextPage && (
-            <Link
-              className="bgi-Footer_column"
-              to={{
-                pathname: blogsPath(),
-                search: `?page=${this.state.currentPage + 1}`
-              }}
-              onClick={() => this.setPosts(this.state.currentPage + 1)}
-            >
-              次のページ
-            </Link>
-          )}
-        </div>
-      </div>
-    );
-  }
+const reducer = (state: State, action: ActionType<State>) => action(state);
 
-  private setPosts(page: number) {
-    getPosts(page).then(
+export const BlogIndex = (props: Props) => {
+  const [state, dispatch] = React.useReducer(
+    reducer,
+    initialState,
+    actions.init(props)
+  );
+
+  React.useEffect(() => {
+    getPosts(state.currentPage).then(
       result =>
-        result.success &&
         result.data &&
         result.data.posts &&
-        this.setState({ posts: result.data.posts, currentPage: page })
+        dispatch(actions.setPosts(result.data.posts))
     );
-  }
-}
+  }, [state.currentPage]);
+
+  return (
+    <div>
+      <Head title={"ブログ"} description={"Gettyのブログ"} url={blogsPath()} />
+      <div className="bgi-Content">
+        <div className="bgi-Content_title">ブログ</div>
+        <div className="bgi-Content_bar" />
+        <div className="bgi-Content_items">
+          {state.posts &&
+            state.posts.edges &&
+            state.posts.edges.map(
+              (post, index) =>
+                post &&
+                post.node && (
+                  <div className="bgi-Content_item" key={index}>
+                    <Link
+                      to={blogPath({ id: post.node.id })}
+                      className="bgi-Content_item-title"
+                    >
+                      {post.node.title}
+                    </Link>
+                    <div className="bgi-Content_item-date">
+                      {formatDate(post.node.insertedAt)}
+                    </div>
+                  </div>
+                )
+            )}
+        </div>
+      </div>
+      <div className="bgi-Footer">
+        {state.posts && state.posts.pageInfo.hasPreviousPage && (
+          <Link
+            className="bgi-Footer_column"
+            to={{
+              pathname: blogsPath(),
+              search: `?page=${state.currentPage - 1}`
+            }}
+            onClick={() =>
+              dispatch(actions.setCurrentPage(state.currentPage - 1))
+            }
+          >
+            前のページ
+          </Link>
+        )}
+        {state.posts && state.posts.pageInfo.hasNextPage && (
+          <Link
+            className="bgi-Footer_column"
+            to={{
+              pathname: blogsPath(),
+              search: `?page=${state.currentPage + 1}`
+            }}
+            onClick={() =>
+              dispatch(actions.setCurrentPage(state.currentPage + 1))
+            }
+          >
+            次のページ
+          </Link>
+        )}
+      </div>
+    </div>
+  );
+};
